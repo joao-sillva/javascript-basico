4e4523f42d27f50026f0f80b1cf02b36890b41fe
joao-sillva <joao.vgsillva@gmail.com>
2024-12-03 22:58:06 -0300

    Case sensitive

---
+++ 02-variaveis/case-sensitive.js
@@ -0,0 +1,9 @@
+/*
+  Uma linguagem case sensitive é quando é sensível
+  a letras maiúsculas e minúsculas, 'Joao' é diferente de 'joao'
+*/
+
+var product = "Teclado Mecânico";
+var Product = "Mouse Gamer";
+console.log(product);
+console.log(Product);
