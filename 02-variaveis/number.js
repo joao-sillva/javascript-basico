94c78dc56d45a507f3f3eefbe180f8febcd4da49
joao-sillva <joao.vgsillva@gmail.com>
2024-12-03 23:04:47 -0300

    Number

---
+++ 02-variaveis/number.js
@@ -0,0 +1,17 @@
+/*
+  - Positivo ou negativo
+  - Reais ou float
+  - Not a Number
+*/
+
+let numberPositive = 22;
+let numberNegative = -22;
+let float = 22.5;
+let notANumber = 22 / "John Doe";
+
+console.log(
+  numberPositive,
+  numberNegative,
+  float,
+  notANumber
+);
