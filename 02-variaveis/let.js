078da4781b146363fde99d35a74f472ebaf209f3
joao-sillva <joao.vgsillva@gmail.com>
2024-12-03 22:58:45 -0300

    Let

---
+++ 02-variaveis/let.js
@@ -0,0 +1,16 @@
+// Declarar variável sem valor
+let user;
+let email;
+console.log(user, email);
+
+
+// Declarar variável com valor
+user = "John Doe";
+email = "johndoe@email.com";
+console.log(user, email);
+
+
+// Substituir valor da variável
+user = "john";
+email = "john@email.com";
+console.log(user, email);
