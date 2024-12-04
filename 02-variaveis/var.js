8eddce2f8d5ffa1ae312e2272029d70fa4f70004
joao-sillva <joao.vgsillva@gmail.com>
2024-12-03 22:57:32 -0300

    Var

---
+++ 02-variaveis/var.js
@@ -0,0 +1,16 @@
+// Declarar variável sem valor
+var user;
+var email;
+console.log(user, email);
+
+
+// Declarar variável com valor
+var user = "John Doe";
+var email = "johndoe@email.com";
+console.log(user, email);
+
+
+// Substituir valor da variável
+user = "John";
+email = "john@email.com";
+console.log(user, email);
