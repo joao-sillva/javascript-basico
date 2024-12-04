d05f381d2eda93892a4eed04489827797663672b
joao-sillva <joao.vgsillva@gmail.com>
2024-12-03 23:04:16 -0300

    Template literals

---
+++ 02-variaveis/template-literals.js
@@ -0,0 +1,7 @@
+// Interpolação de strings
+
+let username = "John Doe";
+let email = "johndoe@email.com";
+let message = `Olá, ${username}. Você conectou com o email ${email} com sucesso!`;
+
+console.log(message);
