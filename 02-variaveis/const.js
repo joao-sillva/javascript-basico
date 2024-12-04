0a6b51b2fbc5987d203b68424bfd68401ce399e5
joao-sillva <joao.vgsillva@gmail.com>
2024-12-03 22:59:15 -0300

    Const

---
+++ 02-variaveis/const.js
@@ -0,0 +1,13 @@
+/*
+  Uma constante cria uma variável cujo valor é fixo
+  (não pode ser alterado)
+*/
+
+const user = "John Doe";
+const email = "Johndoe@email.com";
+console.log(user, email);
+
+
+// Irá gerar um erro por que o valor não pode ser alterado
+user = "john";
+email = "john@email.com";
