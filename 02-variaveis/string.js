ca30512fba67bf4fdb7e42f9640f85ab333a1483
joao-sillva <joao.vgsillva@gmail.com>
2024-12-03 23:04:06 -0300

    String

---
+++ 02-variaveis/string.js
@@ -0,0 +1,12 @@
+let user = "John Doe";
+console.log(user);
+console.log(typeof user);
+
+console.log("Uma string com aspas duplas");
+console.log("Uma string com aspas duplas com 'aspas simples'");
+console.log(`Uma string com apóstrofo e múltiplas linhas
+  John Doe
+  johndoe@email.com
+  22 years
+  04/08/2024
+`);
