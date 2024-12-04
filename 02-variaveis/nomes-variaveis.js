d9d61b4e9c53eaaa3cd1d22c37465c48c88bf604
joao-sillva <joao.vgsillva@gmail.com>
2024-12-03 23:02:13 -0300

    Names for variables

---
+++ 02-variaveis/nomes-variaveis.js
@@ -0,0 +1,17 @@
+// O que pode:
+let username = "John Doe";
+let userName = "John Doe";
+let $username = "John Doe";
+let _username = "John Doe";
+
+console.log(
+  username,
+  userName,
+  $username,
+  _username
+);
+
+// O que não pode:
+let Ação = "Cadastrar";
+let p = "Produto";
+console.log(Ação, p);
