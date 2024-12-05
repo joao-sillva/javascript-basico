85f2f4226665147adadbda5eb189fb64e52dd7f0
joao-sillva <joao.vgsillva@gmail.com>
2024-12-04 21:24:25 -0300

    Greater, lesser and equal

---
+++ 03-expressoes-operadores/maior-menor-igual.js
@@ -0,0 +1,11 @@
+//Maior que, menor que, e maior menor igual a
+
+let balance = 1500;
+let payment = 100;
+
+console.log(`
+  Balance ${balance} é maior que payment ${payment}? ${balance > payment} 
+  Balance ${balance} é menor que payment ${payment}? ${balance < payment} 
+  Balance ${balance} é maior ou igual a payment ${payment}? ${balance >= payment} 
+  Balance ${balance} é menor ou igual a payment ${payment}? ${balance <= payment} 
+`);
