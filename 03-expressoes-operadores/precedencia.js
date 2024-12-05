7513743d53c907ef8f10162d6db1a67a64336363
joao-sillva <joao.vgsillva@gmail.com>
2024-12-04 21:22:01 -0300

    Order of precedence

---
+++ 03-expressoes-operadores/precedencia.js
@@ -0,0 +1,14 @@
+// Ordem de precedência
+
+let value1 = 2 + 3 * 4;
+let value2 = (2 + 3) * 4;
+
+let average1 = 9.5 + 7 + 5 / 3;
+let average2 = (9.5 + 7 + 5) / 3;
+
+console.log(
+  value1,
+  value2,
+  average1,
+  average2
+); // 14, 20, 18.16, 7.16
