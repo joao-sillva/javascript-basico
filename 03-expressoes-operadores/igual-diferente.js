e13632a2682a0fc95a2335b6a2c3da352f47a6f5
joao-sillva <joao.vgsillva@gmail.com>
2024-12-04 21:22:57 -0300

    Equal to and Not Equal to

---
+++ 03-expressoes-operadores/igual-diferente.js
@@ -0,0 +1,16 @@
+let one = 1;
+let two = 2;
+
+// == igual a
+console.log(`
+  ${one} é igual a ${two}? ${one == two} 
+  ${one} é igual a ${one}? ${one == one} 
+  ${one} é igual a "1"? ${one == "1"} 
+`);
+
+// != diferente de
+console.log(`
+  ${one} é diferente de ${two}? ${one != two} 
+  ${one} é diferente de ${one}? ${one != one} 
+  ${one} é diferente de "1"? ${one != "1"} 
+`);
