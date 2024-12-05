f482817d9a53d520a5d2d01c88bdbf8b5050477f
joao-sillva <joao.vgsillva@gmail.com>
2024-12-04 21:19:44 -0300

    Arithmetic operators

---
+++ 03-expressoes-operadores/aritmetico.js
@@ -0,0 +1,16 @@
+/*
+  Soma
+  Subtração
+  Divisão
+  Multiplicação
+  Resto
+*/
+
+console.log(`
+  Soma: ${10 + 5}
+  Subtração: ${10 - 5}
+  Divisão: ${10 / 5}
+  Multiplicação: ${10 * 5}
+  Resto: ${10 % 5}
+  Exponencial: ${10 ** 5}
+`);
