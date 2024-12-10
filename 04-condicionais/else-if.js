034b9d42f3a315af8e824015eb402accd838df9d
joao-sillva <joao.vgsillva@gmail.com>
2024-12-09 21:53:16 -0300

    Else if

---
+++ 04-condicionais/else-if.js
@@ -0,0 +1,11 @@
+let hour = 16
+
+if (hour >= 5 && hour < 12) {
+  console.log('Bom dia!')
+} else if (hour >= 12 && hour < 18) {
+  console.log('Boa tarde!')
+} else if (hour >= 18 && hour < 23) {
+  console.log('Boa noite!')
+} else {
+  console.log('Boa madrugada!')
+}
