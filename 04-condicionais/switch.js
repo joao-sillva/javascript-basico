8647698903b3146b53fe63ad85e043865231287e
joao-sillva <joao.vgsillva@gmail.com>
2024-12-09 21:53:26 -0300

    Switch

---
+++ 04-condicionais/switch.js
@@ -0,0 +1,29 @@
+// Analisar caso a caso
+
+let option = 1
+
+switch (option) {
+  case 1:
+    console.log('Criar pedido')
+    break
+
+  case 2:
+    console.log('Buscar pedido')
+    break
+
+  case 3:
+    console.log('Atualizar pedido')
+    break
+
+  case 4:
+    console.log('Deletar pedido')
+    break
+
+  case 5:
+    console.log('Sair')
+    break
+    
+  default:
+    console.log('Opção inválida')
+    break
+}
