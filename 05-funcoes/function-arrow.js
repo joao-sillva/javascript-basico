7e8baa98fd86e77a2b7b6cd491c4ebad28d6b8a3
joao-sillva <joao.vgsillva@gmail.com>
2024-12-11 20:49:13 -0300

    Function arrow

---
+++ 05-funcoes/function-arrow.js
@@ -0,0 +1,12 @@
+// Função em formato de flecha
+
+const createProfile = () => {
+  console.log('Criar perfil');
+}
+createProfile();
+
+
+const profile = (username, email) => {
+  console.log(`Olá, ${username}. seu e-mail é ${email}`);
+}
+profile('John Doe', 'johndoe@email.com');
